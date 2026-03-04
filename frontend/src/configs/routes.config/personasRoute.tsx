400f57c373532099995ce16b9a34e7c092a3cd4d
wizwiz <abezzi@outlook.cl>
2026-03-04 10:21:58 -0300

    fix(routes): correct key

--- frontend/src/configs/routes.config/personasRoute.tsx
+++ frontend/src/configs/routes.config/personasRoute.tsx
@@ -3,13 +3,13 @@ import type { Routes } from "@/@types/routes";
 
 const personasRoute: Routes = [
   {
-    key: "people",
+    key: "people.dashboard",
     path: `/people/dashboard`,
     component: lazy(() => import("@/views/people/Dashboard")),
     authority: [],
   },
   {
-    key: "people",
+    key: "people.users",
     path: `/people/users`,
     component: lazy(() => import("@/views/people/Users")),
     authority: [],
