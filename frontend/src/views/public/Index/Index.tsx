9ef28b2cdda961fb839b0b7ec2e4f5db822fcd93
wizwiz <abezzi@outlook.cl>
2026-03-06 13:23:03 -0300

    feat(index): template of the index view

---
+++ frontend/src/views/public/Index/Index.tsx
@@ -0,0 +1,27 @@
+const Home = () => {
+  return (
+    <>
+      <div className="mb-8">
+        <h3 className="mb-1">Public home</h3>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+        <p>if you are at localhost:5173/ should see this page</p>
+      </div>
+    </>
+  );
+};
+
+export default Home;
