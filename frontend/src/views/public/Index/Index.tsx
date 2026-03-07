6f743a1a1bae0544338a0b91859232083844c1c8
wizwiz <abezzi@outlook.cl>
2026-03-07 14:37:31 -0300

    chore(index): dummy index view to test if deployment to gcloud is working

--- frontend/src/views/public/Index/Index.tsx
+++ frontend/src/views/public/Index/Index.tsx
@@ -4,21 +4,7 @@ const Home = () => {
       <div className="mb-8">
         <h3 className="mb-1">Public home</h3>
         <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
-        <p>if you are at localhost:5173/ should see this page</p>
+        <p>Google cloud working!</p>
       </div>
     </>
   );
